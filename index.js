66e1e555ccd9b1fca5cab512a73821223179342f
prettykaur <pkaur.dev@gmail.com>
2023-06-05 09:11:18 +0800

    Setup CORS and middleware

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
-import express from "express";
+const cors = require("cors");
+const express = require("express");
+require("dotenv").config();
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const app = express();
 
+// Import routers
+
+// Import controllers
+
+// Import db
+const db = require("./db/models/index");
+const { user } = db;
+
+// Initialise controllers
+
+// Initialise routers
+
+// Enable CORS
+app.use(cors());
+
+// Setup middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Use routers
 app.get("/", (req, res) => {
   res.send("Hello, World!");
 });
