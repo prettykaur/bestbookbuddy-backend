4f849c955b2913315819e90ba386e137b236f287
prettykaur <pkaur.dev@gmail.com>
2023-06-07 18:11:40 +0800

    Create collections controller and router

---
+++ controllers/collectionsController.js
@@ -0,0 +1,25 @@
+const BaseController = require("./baseController");
+
+class CollectionsController extends BaseController {
+  constructor(model) {
+    super(model);
+  }
+
+  // Get all collections
+
+  // Get all of user's collections
+
+  // Get info on specific collection
+
+  // Create new collection
+
+  // Add book to collection
+
+  // Update collection
+
+  // Remove book from collection
+
+  // Delete collection
+}
+
+module.exports = CollectionsController;
