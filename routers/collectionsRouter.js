e054fbaf7d89ad03891a8f2c9e58db9f1de4b64c
prettykaur <pkaur.dev@gmail.com>
2023-06-07 22:18:14 +0800

    Add methods for collections

--- routers/collectionsRouter.js
+++ routers/collectionsRouter.js
@@ -8,6 +8,31 @@ class CollectionsRouter {
   }
 
   routes() {
+    router.get("/", this.controller.getAllCollections);
+    router.post("/", this.checkJwt, this.controller.createCollection);
+
+    router.get("/:userId", this.controller.getUserCollections);
+    router.get("/:userId/:collectionId", this.controller.getCollectionInfo);
+    router.post(
+      "/:userId/:collectionId",
+      this.checkJwt,
+      this.controller.addBookToCollection
+    );
+    router.put(
+      "/:userId/:collectionId",
+      this.checkJwt,
+      this.controller.updateCollection
+    );
+    router.put(
+      "/:userId/:collectionId/:bookId",
+      this.checkJwt,
+      this.controller.removeBookFromCollection
+    );
+    router.delete(
+      "/:userId/:collectionId",
+      this.checkJwt,
+      this.controller.deleteCollection
+    );
     return router;
   }
 }
